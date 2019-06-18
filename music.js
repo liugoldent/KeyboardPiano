8d3875855559da7b3d689e177de146eded243d69
GT Liu <liugoldent@gmail.com>
2019-06-19 00:37:49 +0800

    add Music all

---
+++ music.js
@@ -0,0 +1,306 @@
+function GoFunction() {
+	switch(event.keyCode) {
+//高音域down
+        //Q
+        case 81:
+        var audio = document.getElementById("btnMusicQ");
+        $("#Q").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //W
+        case 87:
+        var audio = document.getElementById("btnMusicW");
+        $("#W").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //E
+        case 69:
+        var audio = document.getElementById("btnMusicE");
+        $("#E").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //R
+        case 82:
+        var audio = document.getElementById("btnMusicR");
+        $("#R").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //T
+        case 84:
+        var audio = document.getElementById("btnMusicT");
+        $("#T").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //Y
+        case 89:
+        var audio = document.getElementById("btnMusicY");
+        $("#Y").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //U
+        case 85:
+        var audio = document.getElementById("btnMusicU");
+        $("#U").css({"background-color":"pink"});
+        audio.play();
+        break;
+//高音域up
+            
+            
+//中音域down
+        //A
+        case 65:
+        var audio = document.getElementById("btnMusicA");
+        $("#A").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //S
+        case 83:
+        var audio = document.getElementById("btnMusicS");
+        $("#S").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //D
+        case 68:
+        var audio = document.getElementById("btnMusicD");
+        $("#D").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //F
+        case 70:
+        var audio = document.getElementById("btnMusicF");
+        $("#F").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //G
+        case 71:
+        var audio = document.getElementById("btnMusicG");
+        $("#G").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //H
+        case 72:
+        var audio = document.getElementById("btnMusicH");
+        $("#H").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //J
+        case 74:
+        var audio = document.getElementById("btnMusicJ");
+        $("#J").css({"background-color":"pink"});
+        audio.play();
+        break;    
+//中音域up
+            
+//低音域down
+        //Z
+        case 90:
+        var audio = document.getElementById("btnMusicZ");
+        $("#Z").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //X
+        case 88:
+        var audio = document.getElementById("btnMusicX");
+        $("#X").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //C
+        case 67:
+        var audio = document.getElementById("btnMusicC");
+        $("#C").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //V
+        case 86:
+        var audio = document.getElementById("btnMusicV");
+        $("#V").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //B
+        case 66:
+        var audio = document.getElementById("btnMusicB");
+        $("#B").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //N
+        case 78:
+        var audio = document.getElementById("btnMusicN");
+        $("#N").css({"background-color":"pink"});
+        audio.play();
+        break;
+        //M
+        case 77:
+        var audio = document.getElementById("btnMusicM");
+        $("#M").css({"background-color":"pink"});
+        audio.play();
+        break;    
+//低音域up
+    }
+}
+
+
+function UpFunction() {
+	switch (event.keyCode) {
+//高音域down
+        //Q
+        case 81:
+        var audio = document.getElementById("btnMusicQ");
+        $("#Q").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();
+            break;
+        //W
+        case 87:
+        var audio = document.getElementById("btnMusicW");
+        $("#W").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause(); 
+            break;
+        //E
+        case 69:
+        var audio = document.getElementById("btnMusicE");
+        $("#E").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+        //R
+        case 82:
+        var audio = document.getElementById("btnMusicR");
+        $("#R").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();
+            break;
+        //T
+        case 84:
+        var audio = document.getElementById("btnMusicT");
+        $("#T").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause(); 
+            break;
+        //Y
+        case 89:
+        var audio = document.getElementById("btnMusicY");
+        $("#Y").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+        //U
+        case 85:
+        var audio = document.getElementById("btnMusicU");
+        $("#U").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+//高音域up
+            
+            
+//中音域down
+            
+        //A
+        case 65:
+        var audio = document.getElementById("btnMusicA");
+        $("#A").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();
+            break;
+        //S
+        case 83:
+        var audio = document.getElementById("btnMusicS");
+        $("#S").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause(); 
+            break;
+        //D
+        case 68:
+        var audio = document.getElementById("btnMusicD");
+        $("#D").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+        //F
+        case 70:
+        var audio = document.getElementById("btnMusicF");
+        $("#F").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();
+            break;
+        //G
+        case 71:
+        var audio = document.getElementById("btnMusicG");
+        $("#G").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause(); 
+            break;
+        //H
+        case 72:
+        var audio = document.getElementById("btnMusicH");
+        $("#H").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+        //J
+        case 74:
+        var audio = document.getElementById("btnMusicJ");
+        $("#J").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+//中音域up
+            
+//低音域down
+            
+        //Z
+        case 90:
+        var audio = document.getElementById("btnMusicZ");
+        $("#Z").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();
+            break;
+        //X
+        case 88:
+        var audio = document.getElementById("btnMusicX");
+        $("#X").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause(); 
+            break;
+        //C
+        case 67:
+        var audio = document.getElementById("btnMusicC");
+        $("#C").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+        //V
+        case 86:
+        var audio = document.getElementById("btnMusicV");
+        $("#V").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();
+            break;
+        //B
+        case 66:
+        var audio = document.getElementById("btnMusicB");
+        $("#B").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause(); 
+            break;
+        //N
+        case 78:
+        var audio = document.getElementById("btnMusicN");
+        $("#N").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+        //M
+        case 77:
+        var audio = document.getElementById("btnMusicM");
+        $("#M").css({"background-color":"white"});
+        audio.currentTime=0;
+        audio.pause();      
+            break;
+//低音域up
+    
+    }
+}
+document.onkeydown=GoFunction;
+document.onkeyup=UpFunction;
